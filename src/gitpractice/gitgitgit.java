769881a799f457a0772afc38b189fb4807332c99
egemendenizbahar <mertonurbahar0912g>
2019-07-31 21:30:16 -0500

    ghjhg

--- src/gitpractice/gitgitgit.java
+++ src/gitpractice/gitgitgit.java
@@ -4,7 +4,6 @@ public class gitgitgit {
 
     public static void main(String[] args) {
         System.out.println("Hi Azamat");
-        System.out.println("Hi Deniz");
-        System.out.println("hihihihih");
+
     }
 }
