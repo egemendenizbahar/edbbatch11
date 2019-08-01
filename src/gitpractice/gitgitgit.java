58328acde0b2198a432ecfb261b8681bf5d3e175
egemendenizbahar <mertonurbahar0912g>
2019-07-31 21:31:30 -0500

    faf

--- src/gitpractice/gitgitgit.java
+++ src/gitpractice/gitgitgit.java
@@ -5,7 +5,6 @@ public class gitgitgit {
     public static void main(String[] args) {
         System.out.println("Hi Azamat");
         System.out.println("Hi Deniz");
-        System.out.println("hihihihih");
         System.out.println("lkjljjkj");
     }
 }
